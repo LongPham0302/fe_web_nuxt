ea41a9281e98f2bc04ab4fa6a53b11721fd78da1
LongPham0302 <bekeo0302@gmail.com>
2024-01-23 19:43:15 +0700

    fixbug

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:4000',
+    baseURL: 'https://be-web-p8nb.onrender.com/',
     headers: {
         'accept': 'application/json'
     }
