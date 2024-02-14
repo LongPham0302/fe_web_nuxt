f7347e22cb3d42739c72047d6d88f08f372af4a9
LongPham0302 <bekeo0302@gmail.com>
2024-02-14 16:30:31 +0700

    add code

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 const apiUrl = process.env.apiUrl; // Lấy apiUrl từ biến môi trường
-console.log("apiUrl", apiUrl);
-
 const axiosInstance = axios.create({
   baseURL: apiUrl,
   headers: {
